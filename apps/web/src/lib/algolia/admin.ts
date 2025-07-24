62a4695e68721ec20e9a644d92648b8d267b7848
Rishit <104666906+Rish-it@users.noreply.github.com>
2025-07-24 18:07:16 +0530

    refactor

--- apps/web/src/lib/algolia/admin.ts
+++ apps/web/src/lib/algolia/admin.ts
@@ -1,9 +1,10 @@
 import { algoliasearch } from 'algoliasearch';
 
 const client = algoliasearch(
-  process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!,
-  process.env.ALGOLIA_ADMIN_API_KEY!
+  process.env.ALGOLIA_APP_ID!,
+  process.env.ALGOLIA_ADMIN_API_KEY!,
 );
 
-export const ALGOLIA_INDEX_NAME = process.env.ALGOLIA_INDEX_NAME || 'blog_posts';
+export const ALGOLIA_INDEX_NAME = 
+  process.env.ALGOLIA_INDEX_NAME || "blog_posts";
 export { client as algoliaClient };
